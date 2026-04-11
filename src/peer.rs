7510e9003049f15f8c15f0ca6f52c9a9c19677df
Bartosz Bachul <bartoszbachul1@gmail.com>
2026-04-11 18:04:28 +0200

    Send handshake to remote peer

--- src/peer.rs
+++ src/peer.rs
@@ -31,9 +31,11 @@ impl Peer {
         self.interested
     }
 
-    pub fn connect(&mut self) -> io::Result<()> {
+    pub fn connect(&mut self, info_hash: &[u8], client_id: &[u8]) -> io::Result<()> {
         let mut connection = PeerConnection::new(&self.socket)?;
         println!("Connected to peer: {}", self.socket);
+
+        connection.send_handshake(info_hash, client_id)?;
 
         Ok(())
     }
@@ -51,5 +53,34 @@ impl PeerConnection {
             stream: TcpStream::connect_timeout(socket, Duration::from_secs(5))?,
             handshake_sent: false,
         })
+    }
+
+    fn send_bytes(&mut self, bytes: &[u8]) -> io::Result<()> {
+        Ok(self.stream.write_all(bytes)?)
+    }
+
+    fn encode_handshake(info_hash: &[u8], client_id: &[u8]) -> Vec<u8> {
+        let mut handshake = Vec::with_capacity(68);
+
+        handshake.push(19);
+        handshake.extend_from_slice(b"BitTorrent protocol");
+        handshake.extend_from_slice(&[0u8; 8]);
+        handshake.extend_from_slice(info_hash);
+        handshake.extend_from_slice(client_id);
+
+        handshake
+    }
+
+    fn send_handshake(&mut self, info_hash: &[u8], client_id: &[u8]) -> io::Result<()> {
+        if self.handshake_sent {
+            return Ok(());
+        }
+
+        self.send_bytes(&Self::encode_handshake(info_hash, client_id))?;
+        self.handshake_sent = true;
+
+        println!("-> handshake");
+
+        Ok(())
     }
 }
